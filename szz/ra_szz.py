16038ef7cf33835947f6aaa606b2d4a40eb020a5
Peter Bludau <bludau@fortiss.org>
2021-03-15 20:34:13 +0100

    c

--- szz/ra_szz.py
+++ szz/ra_szz.py
@@ -20,39 +20,38 @@ class RASZZ(MASZZ):
 
     def __init__(self, repo_full_name: str, repo_url: str, repos_dir: str = None):
         super().__init__(repo_full_name, repo_url, repos_dir)
+        self.refactorings = dict()
         
     def _extract_refactorings(self, commits):
         PATH_TO_REFMINER = os.path.join(Options.PYSZZ_HOME, 'tools/RefactoringMiner-2.0/bin/RefactoringMiner')
         
-        refactorings = dict()
         for commit in commits:
-            if not commit in refactorings:
+            if not commit in self.refactorings:
                 log.info(f'Running RefMiner on {commit}')
                 command = [PATH_TO_REFMINER, "-c", self._repository_path, commit]
                 try:
                     out = subprocess.check_output(command, stderr=subprocess.DEVNULL, timeout=300)
                     data = json.loads(out.decode('utf-8')) 
                     if 'commits' in data and len(data['commits']) > 0 and 'refactorings' in data['commits'][0]:
-                        refactorings[commit] = data['commits'][0]['refactorings'] 
+                        self.refactorings[commit] = data['commits'][0]['refactorings'] 
                     else:
                         log.info("Refactoring format corrupted for commit: {}".format(commit))
-                        refactorings[commit] = []
+                        self.refactorings[commit] = []
                     #p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                 except subprocess.CalledProcessError as e:
                     log.error(e)
                 except subprocess.TimeoutExpired as e:
                     log.error("Command timed out: {}".format(e))
-                    refactorings[commit] = []
-        return refactorings
+                    self.refactorings[commit] = []
     
     def get_impacted_files(self, fix_commit_hash: str,
                            file_ext_to_parse: List[str] = None,
                            only_deleted_lines: bool = True) -> List['ImpactedFile']:
         impacted_files = set(super().get_impacted_files(fix_commit_hash, file_ext_to_parse, only_deleted_lines))
         
-        fix_refactorings = self._extract_refactorings([fix_commit_hash])
+        self._extract_refactorings([fix_commit_hash])
         
-        for refactoring in fix_refactorings[fix_commit_hash]:
+        for refactoring in self.refactorings[fix_commit_hash]:
             for location in refactoring['rightSideLocations']:
                 file_path = location['filePath']
                 from_line = location['startLine']
@@ -67,7 +66,7 @@ class RASZZ(MASZZ):
         
         impacted_files = [f for f in impacted_files if len(f.modified_lines) > 0]
         return impacted_files
-        
+  
     def _blame(self, 
                rev: str,
                file_path: str,
@@ -79,7 +78,6 @@ class RASZZ(MASZZ):
                detect_move_within_file: bool = False,
                detect_move_from_other_files: 'DetectLineMoved' = None
                ) -> Set['BlameData']:
-        
         log.info("Running super-blame")
         candidate_blame_data = super()._blame(
             rev,
@@ -94,14 +92,14 @@ class RASZZ(MASZZ):
         )
         
         commits = set([blame.commit.hexsha for blame in candidate_blame_data])
-        blame_refactorings = self._extract_refactorings(commits)
+        self._extract_refactorings(commits)
         
         to_reblame = dict()
         
         result_blame_data = set()
         for blame in candidate_blame_data:
             can_add = True
-            for refactoring in blame_refactorings[blame.commit.hexsha]:
+            for refactoring in self.refactorings[blame.commit.hexsha]:
                 for location in refactoring['rightSideLocations']:
                     file_path = location['filePath']
                     from_line = location['startLine']
@@ -112,7 +110,7 @@ class RASZZ(MASZZ):
                         if not (blame.commit.hexsha + "@" + blame.file_path) in to_reblame:
                             to_reblame[blame.commit.hexsha + "@" + blame.file_path] = ReblameCandidate(blame.commit.hexsha, blame.file_path, [blame.line_num])
                         else:
-                            to_reblame[blame.commit.hexsha + "@" + blame.file_path].modified_lines.add(blame.line_num)
+                            to_reblame[blame.commit.hexsha + "@" + blame.file_path].modified_lines.append(blame.line_num)
                         can_add = False
                     
             if can_add:
@@ -120,7 +118,8 @@ class RASZZ(MASZZ):
                 
         for _, reblame_candidate in to_reblame.items():
             log.info(f'Re-blaming {reblame_candidate.file_path} @ {reblame_candidate.rev}, lines {reblame_candidate.modified_lines} because of refactoring')
-            
+            if reblame_candidate.rev in ignore_revs_list:
+                continue
             new_ignore_revs_list = ignore_revs_list.copy()
             new_ignore_revs_list.append(reblame_candidate.rev)
             
